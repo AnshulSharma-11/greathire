ea8e80d3f71ef9d0e9e48033cd1bd7ccef244508
Manas patil <patilmanas481@gmail.com>
2026-07-07 12:15:58 +0530

    Add attendance and reports routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "@/pages/LoginPage";
 import DashboardPage from "@/pages/DashboardPage";
 import EmployeeDashboardPage from "@/pages/EmployeeDashboardPage";
+import AttendanceManagement from "@/pages/AttendanceManagement";
+import Report from "@/pages/Report";
 
 export default function App() {
   return (
@@ -9,6 +11,8 @@ export default function App() {
       <Route path="/" element={<LoginPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/employee-dashboard" element={<EmployeeDashboardPage />} />
+      <Route path="/attendance" element={<AttendanceManagement />} />
+      <Route path="/reports" element={<Report />} />
     </Routes>
   );
 }
