abbd9957542eb56a2200103fdc86c96f0062a823
AnshulSharma-11 <anshuls12120@gmail.com>
2026-07-31 17:17:11 +0530

    v2

--- backend/src/utils/ApiResponse.js
+++
@@ -1,8 +0,0 @@
-export class ApiResponse {
-  constructor(statusCode, data = null, message = "Success") {
-    this.statusCode = statusCode;
-    this.success = statusCode < 400;
-    this.message = message;
-    this.data = data;
-  }
-}
--- frontend/src/lib/api/messages.js
+++
@@ -1,9 +0,0 @@
-import { api } from "../apiClient.js";
-
-export const messagesApi = {
-  listConversations: () => api.get("/messages/conversations"),
-  getConversation: (id) => api.get(`/messages/conversations/${id}`),
-  listMessages: (id) => api.get(`/messages/conversations/${id}/messages`),
-  sendMessage: (id, content, attachments) => api.post(`/messages/conversations/${id}/messages`, { content, attachments }),
-  markRead: (id) => api.post(`/messages/conversations/${id}/read`),
-};
